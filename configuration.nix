2090a7ba130b2b98acdfad488b164b756db8cff7
Josh Callanan <joshua.callanan@pm.me>
2023-02-11 10:23:15 +0800

    exclude xterm

--- configuration.nix
+++ configuration.nix
@@ -43,11 +43,14 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    excludePackages = [ pkgs.xterm ];
+    
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
