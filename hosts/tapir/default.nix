8a1910c27fa8cd1a2f86c99455758d8212487292
Josh Callanan <josh@callanan.contact>
2026-08-27 19:59:25 +0800

    some emacs fixes and remove redundant user definition on tapir

--- hosts/tapir/default.nix
+++ hosts/tapir/default.nix
@@ -10,17 +10,6 @@
     ../../modules/workstation
   ];
 
-  # Users
-
-  # users.users.josh = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" "networkmanager" ];
-  #   hashedPassword = "$6$0KOODrlgZ8LGrmKe$.fS3JbK3ey4HCOQozYhhkT21YsxM/m80FUkuB47HsN7F1ILrgYNsIriLUd0/VXhRFdm9VE2WJ2eOUkV9g.ILf/";
-  #   openssh.authorizedKeys.keys = [
-  #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTkf9WjAcV3S2iHravn1okBw3YK81s/YjGr2kLyh6+j josh@callanan.contact"
-  #   ];
-  # };
-
   # Display Manager & Desktop Manager
 
   services = {
